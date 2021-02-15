93388026211a2f5c9d2cebb7f70ecb8db7bf2e1a
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-15 23:30:24 +0900

    이미지 추가 컴포넌트 작업

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,11 @@
-import { PageComponet } from './component/page.js';
+import { ImageComponent } from './component/page/item/image.js';
+import { PageComponet } from './component/page/page.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponet();
         this.page.attachTo(appRoot);
+        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
-import { PageComponet } from './component/page.js';
+import { ImageComponent } from './component/page/item/image.js';
+import { PageComponet } from './component/page/page.js';
+
 class App {
     private readonly page: PageComponet;
 
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponet();
         this.page.attachTo(appRoot);
+       
+        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
     }
 }
 
-new App(document.querySelector('.document') as HTMLElement);
+new App(document.querySelector('.document')! as HTMLElement);

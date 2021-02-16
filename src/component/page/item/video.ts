84bb4a86d42b1810308a767696b780fea003aef2
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-16 20:27:30 +0900

    Video class 구현

--- src/component/page/item/video.ts
+++ src/component/page/item/video.ts
@@ -14,13 +14,28 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
         );
 
         const iframe = this.element.querySelector('.video__iframe')! as HTMLVideoElement;
-        url = "https://www.youtube.com/embed/fBw5iwzu7hI"
-        iframe.src = url;
-
+        iframe.src = this.convertToEmbeddedURL(url);
+        
         const videoTitle = this.element.querySelector('.video__title')! as HTMLHeadElement;
         videoTitle.textContent = title;
     }
-}
+    // input
+    // https://www.youtube.com/embed/fBw5iwzu7hI
+    // https://youtu.be/K3-jG52XwuQ
+    // output
+    // 정규표현식 Regex
+    private convertToEmbeddedURL(url: string): string {
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp);
+
+        const videoId = match ? match[1] || match[2] : undefined;
+        
+        if(videoId) {
+            return `https://youtu.be/${videoId}`;
+        }
+        return url;
+    }
+}   
 
 // <iframe
 //             width="1237"

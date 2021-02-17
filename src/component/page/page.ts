b438038882a5305b26560a9a11b5e6fc347d413f
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-17 22:17:27 +0900

    Close 기능 구현

--- src/component/page/page.ts
+++ src/component/page/page.ts
@@ -5,33 +5,49 @@ export interface Composable {
    addChild(child: Componet): void;
 }
 
+type OnCloseListener = () => void;
+
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+    private closeListener?: OnCloseListener 
+
     constructor() {
         super(
             `<li class="page-item">
                 <section class="page-item__body">
                     <div class="page-item__controls">
-                        <span class="close"> &times; </span>
+                        <button class="close"> &times; </button>
                     </div>
                 </section>
-            </li>`
-        )
+            </li>`);
+
+        const closeButton = this.element.querySelector(".close")! as HTMLButtonElement;
+        closeButton.onclick = () => {
+            this.closeListener && this.closeListener();
+        }
     }
 
     addChild(child: Componet) {
         const container = this.element.querySelector('.page-item__body')! as HTMLElement;
         child.attachTo(container);
     }
+
+    setOnCloseListener(listener: OnCloseListener) {
+        this.closeListener = listener;
+        console.log('listener', listener);
+    }
 }
 
 export class PageComponet extends BaseComponent<HTMLUListElement> implements Composable {
     constructor() {
-        super('<ul class="page">/ul>');
+        super('<ul class="page"></ul>');
     }
 
     addChild(section: Componet) {
         const item = new PageItemComponent();
         item.addChild(section);
         item.attachTo(this.element, 'beforeend');
+        item.setOnCloseListener(() => {
+            item.removeFrom(this.element);
+        });
     }
 }

14d1c839b63b63246afef1c7d26ee0eaff1f3780
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-03-01 22:10:45 +0900

    Video Class convertToEmbeddedURL youtube embed id 추출 함수 생성

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -16,13 +16,29 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
         )
 
         const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-        iframe.src = "https://www.youtube.com/embed/tetFlvjO2G0"; // url -> videoId => embed
-        console.log(url);
+        iframe.src =  this.convertToEmbeddedURL(url); // url -> videoId => embed
         
         const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
         titleElement.textContent = title;
     }
+
+    private convertToEmbeddedURL(url: string): string{
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp);
+    
+        const videoId = match ? match[1] || match[2] : undefined;
+        if(videoId) {
+            return `https://www.youtube.com/embed/${videoId}`;
+        }
+        return url;
+    }
 }
+
+// input
+// https://www.youtube.com/embed/tetFlvjO2G0
+// https://youtu.be/tFlvjO2G0
+// outout
+// https://www.youtube.com/embed/tFlvjO2G0
 
 // <iframe
 // width="1280"
